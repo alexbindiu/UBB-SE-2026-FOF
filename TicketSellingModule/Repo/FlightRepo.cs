ffb6883d7be4448836105efcac9114ae29e05ba0
PopaSamuelPaul <samuel.popa@stud.ubbcluj.ro>
2026-03-20 10:37:37 +0200

    added the flight repo

---
+++ TicketSellingModule/Repo/FlightRepo.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using TicketSellingModule.Domain;
+
+namespace TicketSellingModule.Repo
+{
+	public class FlightRepository
+	{
+	}
+}
