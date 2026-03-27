c691e121e9fd401a132ef2b8b613a9476e424e93
teosuciu04 <teosuciu2006@gmail.com>
2026-03-27 18:14:41 +0200

    Colors changed

--- TicketSellingModule/Repo/DbConnectionFactory.cs
+++ TicketSellingModule/Repo/DbConnectionFactory.cs
@@ -10,7 +10,7 @@ namespace TicketSellingModule.Repo
         public DbConnectionFactory()
         {
             // This is the exact string we verified earlier for your specific machine
-            _connectionString = "Server=DESKTOP-K094GD0\\SQLEXPRESS;Database=AirportDB;Trusted_Connection=True;TrustServerCertificate=True;";
+            _connectionString = "Server=localhost;Database=AirportDB;Trusted_Connection=True;TrustServerCertificate=True;";
         }
 
         // Every repository will call this method when it needs to talk to the database
