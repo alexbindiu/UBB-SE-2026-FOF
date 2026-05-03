d7711d3c3cf7be2ed62312c0823c7dd8712a7e93
Ionut2208 <beneionutmatei@gmail.com>
2026-05-03 16:17:48 +0300

    modified AirportDashboard, StaffLogin and StaffPage ViewModels

--- src/TicketSellingModule/ViewModel/StaffLoginViewModel.cs
+++ src/TicketSellingModule/ViewModel/StaffLoginViewModel.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
@@ -9,13 +12,56 @@ namespace TicketSellingModule.ViewModel
 {
     public partial class StaffLoginViewModel(
         IEmployeeService employeeService,
-        INavigationService navigationService) : ObservableObject
+        INavigationService navigationService) : INotifyPropertyChanged
     {
         private const string ErrorMessageFailedLogin = "Failed Login";
 
-        [ObservableProperty] private string employeeIdText;
-        [ObservableProperty] private string errorMessage;
-        [ObservableProperty] private Visibility errorVisibility = Visibility.Collapsed;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string employeeIdText;
+
+        public string EmployeeIdText
+        {
+            get => employeeIdText;
+            set
+            {
+                if (employeeIdText != value)
+                {
+                    employeeIdText = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string errorMessage;
+
+        public string ErrorMessage
+        {
+            get => errorMessage;
+            set
+            {
+                if (errorMessage != value)
+                {
+                    errorMessage = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private Visibility errorVisibility = Visibility.Collapsed;
+
+        public Visibility ErrorVisibility
+        {
+            get => errorVisibility;
+            set
+            {
+                if (errorVisibility != value)
+                {
+                    errorVisibility = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
         [RelayCommand]
         private void Login()
@@ -39,6 +85,11 @@ namespace TicketSellingModule.ViewModel
         {
             ErrorMessage = message;
             ErrorVisibility = Visibility.Visible;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
